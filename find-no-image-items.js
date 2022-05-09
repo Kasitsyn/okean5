c372c3f88cb597a6fff0c22a32d47e2813b1dac3
Yura <kasitsyn@gmail.com>
2022-05-09 13:06:40 +0700

    feat: create find-no-image-items.js
    worked with okean5.ru site

---
+++ find-no-image-items.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://okean5.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=okean5.ru
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const noImg = []
+    renderStartBtn()
+    function renderStartBtn() {
+        const startBtn = document.createElement('button')
+        startBtn.classList.add('btn', 'btn-default')
+
+        startBtn.innerText = 'Товар без фото'
+        document.getElementsByTagName('body')[0].prepend(startBtn)
+        startBtn.onclick = () => {
+            findNoImgItems().then( () => renderNoImgItems())
+
+        }
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function findNoImgItems() {
+        let pagination = document.querySelectorAll('.pagination li')
+        for (let page = 0; page < pagination.length - 2; page++ ) {
+
+            document.querySelector('.ajax_load_btn').click()
+            await sleep(500);
+
+        }
+
+        let items = document.querySelectorAll(".item")
+        let noImgItems = document.querySelectorAll('[src="/bitrix/templates/asproallcorp2/images/noimage_product.png"]')
+        items.forEach( item => {
+            noImgItems.forEach( noImgItem => {
+                item.contains(noImgItem) ? noImg.push(item) : ''
+            })
+        })
+    }
+
+    function renderNoImgItems() {
+        let itemsContainer = document.querySelector(".items")
+        itemsContainer.innerHTML = ''
+        !!noImg.length ? noImg.forEach( item => itemsContainer.append(item) ) : ''
+    }
+
+})();
